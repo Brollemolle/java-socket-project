b2b4fab1d464c8eaa69bcfb49c574fb33bb7a059
Brollemolle <brolinadam5@gmail.com>
2025-02-02 11:16:08 +0100

    Removed all submodules but kept files.

---
+++ tcp-ask/tcpclient/TCPClient.java
@@ -0,0 +1,18 @@
+package tcpclient;
+
+import java.io.IOException;
+
+public class TCPClient {
+    
+    public TCPClient() {
+    }
+
+    public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
+        return new byte[0];
+    }
+
+    public byte[] askServer(String hostname, int port) throws IOException {
+        return new byte[0];
+    }
+
+}
